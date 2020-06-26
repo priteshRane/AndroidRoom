f24dc55a41255b45e2504a7b6eee37d2247350c1
Pritesh_PC <priteshrane01@gmail.com>
2020-06-26 21:53:07 +0530

    Insert operation, Async task for insert operation

--- app/src/main/java/com/ransoft/androidroom/simplifiedcoading/data/db/entities/Note.kt
+++ app/src/main/java/com/ransoft/androidroom/simplifiedcoading/data/db/entities/Note.kt
@@ -4,10 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+const val CURRENT_NOTE_ID = 0
+
 @Entity
 data class Note(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
     val title: String,
     val note: String
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = CURRENT_NOTE_ID
+}
--- app/src/main/java/com/ransoft/androidroom/simplifiedcoading/ui/home/AddNoteFragment.kt
+++ app/src/main/java/com/ransoft/androidroom/simplifiedcoading/ui/home/AddNoteFragment.kt
@@ -1,13 +1,17 @@
 package com.ransoft.androidroom.simplifiedcoading.ui.home
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.ransoft.androidroom.R
 import com.ransoft.androidroom.simplifiedcoading.data.db.NoteDatabase
+import com.ransoft.androidroom.simplifiedcoading.data.db.entities.Note
+import com.ransoft.androidroom.simplifiedcoading.util.toast
 import kotlinx.android.synthetic.main.fragment_add_note.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -23,11 +27,39 @@ class AddNoteFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        NoteDatabase(requireActivity())
-
         btn_done.setOnClickListener {
-            val action = AddNoteFragmentDirections.actionSaveNote()
-            Navigation.findNavController(it).navigate(action)
+            val noteTitle = tie_title.text.toString().trim()
+            val noteBody: String = tie_note.text.toString().trim()
+
+            if (noteTitle.isNullOrEmpty()) {
+                tie_title.error = "title required"
+                tie_title.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (noteBody.isNullOrEmpty()) {
+                tie_note.error = "note required"
+                tie_note.requestFocus()
+                return@setOnClickListener
+            }
+
+            val note = Note(noteTitle, noteBody)
+            saveNote(note)
         }
+    }
+
+    private fun saveNote(note: Note) {
+        class SaveNote : AsyncTask<Void, Void, Void>(){
+            override fun doInBackground(vararg params: Void?): Void? {
+                NoteDatabase(requireActivity()).getNoteDao().addNote(note)
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+                requireActivity().toast("Note saved")
+            }
+        }
+        SaveNote().execute()
     }
 }
