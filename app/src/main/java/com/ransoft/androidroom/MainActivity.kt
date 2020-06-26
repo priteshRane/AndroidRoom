9ea8970b48f22b4713c46851b33534ef0850e7d8
Pritesh_PC <priteshrane01@gmail.com>
2020-06-26 20:36:56 +0530

    Project setup is done

--- app/src/main/java/com/ransoft/androidroom/MainActivity.kt
+++ app/src/main/java/com/ransoft/androidroom/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.ransoft.androidroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ransoft.androidroom.databinding.ActivityMainBinding
+import com.ransoft.androidroom.simplifiedcoading.HomeActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnSimplifiedCoading.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/ransoft/androidroom/simplifiedcoading/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.ransoft.androidroom.simplifiedcoading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ransoft.androidroom.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
